06c4e53b138f15ab2852b430fb7e65a231ca4747
Daniel Thompson <danielthompson@FahrenhitsiMac2.attlocal.net>
2022-01-03 09:05:23 -0600

    added binary search iterative method

---
+++ binarysearch.js
@@ -0,0 +1,24 @@
+// Given an array of integers 'nums' which is sorted in ascending order, and an integer 'target,' write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+var search = function(nums, target) {
+	let left = 0
+	let right = nums.length-1
+	
+	while ( right >= left ) {
+		let mid = Math.floor( (left + right)/2 )
+		if ( target == nums[mid] ) {
+			return mid
+		}
+		else if ( target < nums[mid] ) {
+			right = mid -1
+		}
+		else if ( target > nums[mid] ) {
+			left = mid + 1
+		}
+		else {
+			return -1
+		}
+	}
+	return -1
+};
